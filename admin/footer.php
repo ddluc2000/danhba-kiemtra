b8fd435b85439f8ce20a97d141a364b17e40acda
Lực <62415755+ddluc2000@users.noreply.github.com>
2021-08-24 10:35:20 +0700

    three commit

--- admin/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-                <div class="col-md-12 text-center">
-                    <a href="#" class="text-decoration-none text-dark">&copy; 2021 All rights reserved, Food House. Developed By - DDLUC</a>
-                </div>
-            </footer>
-        </div>
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-</html>
--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include("config/db.php");
-    session_destroy();
-    header('location:'.SITEURL);
-?>
-<h3>abc</h3>
